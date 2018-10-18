b96c6adaf8e24f2143ea93b65d8de8237ade6045
james henry <aengrhenry@gmail.com>
2018-10-18 15:43:49 +0000

    setup the register process and hash the password ,store them in database

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = required("bcryptjs");
 
 //bring in the user model
 let User = require("../models/users");
@@ -15,6 +16,53 @@ router.post("/register", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
   const password2 = req.body.password2;
+
+  check("name", "name is required")
+    .trim()
+    .not(),
+    check("email")
+      .not()
+      .exists()
+      .withMessage("email is required"),
+    check("email")
+      .isEmail()
+      .withMessage("invalid email"),
+    check("password")
+      .not()
+      .exists()
+      .withMessage("password is required"),
+    check("password2")
+      .equals("req.body.password")
+      .withMessage("password do not match");
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.render("register", {
+      errors: errors
+    });
+  } else {
+    let newUser = new User({
+      name: name,
+      email: email,
+      password: password
+    });
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) {
+          console.log(err);
+        }
+        newUser.password = hash;
+        newUser.save(err => {
+          if (err) {
+            console.log(err);
+          } else {
+            req.flash("success", "you are now registerd and can login");
+            res.redirect("/users/login");
+          }
+        });
+      });
+    });
+  }
 });
 
 module.exports = router;
